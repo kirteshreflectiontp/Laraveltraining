a7fcb6f598d16547ba73f1d52c932b2d60f57096
kirteshreflectiontp <kirtesh.reflection@gmail.com>
2024-10-08 10:31:52 +0530

    laravel First

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LaraApp</title>
+</head>
+<body>
+    <div Class="Navbar">
+     <h1>Home</h1>
+     <h1>About</h1>
+     <h1>Products</h1>
+     <h1>Gallery</h1>
+     <h1>Contact</h1>
+</div>
+        
+</body>
+</html>
+    </body>
+</html>
